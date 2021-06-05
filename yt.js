fdfecfab7b22e40519016fdb01a597789efb48d8
Toni Kukurin <tkukurin@gmail.com>
2021-06-05 13:00:37 +0200

    add support for persistent caption tracking within yt session

--- yt.js
+++ yt.js
@@ -11,30 +11,27 @@
 // ==/UserScript==
 
 (function() {
-  const captionTracker = new CaptionTracker(
-    100, '#player-container', '.captions-text');
-  window.captionTracker = captionTracker;
-
-  function copyUrl() {
+  function copyUrl(withCaptions) {
     function timeToUrl(timeStr) {
       const id = /\?v\=([^&]+)/g.exec(window.location.search);
       const url = (id && `https://youtu.be/${id[1]}?t=${timeStr}`) ||
         window.location.href.replace(/&t\=[^&]+/, `&t=${timeStr}`);
       return {t: timeStr, url: url};
     }
-    // NOTE(tk) could also just use ?t=vid.currentTime
-    // but having h/m/s is semantically nicer
+    const tracker = window.captionTracker;
+    const maybeCaptions = withCaptions && tracker ? tracker.get() : '';
+    // NOTE(tk) `?t=vid.currentTime` also works but h/m/s is semantically nicer
     Q.doc('video')
       .then(vid => vid.currentTime)
       .then(time => [time/3600, (time/60)%60, (time%60)])
       .then(ts => ts.map(n => parseInt(n)))
-      .then(ts => ts.map((n, i) => n ? `${n}${'hms'[i]}`.padStart(3, 0) : ''))
+      .then(ts => ts.map((n, i) => n ? `${n}${'hms'[i]}` : ''))
       .then(tsWithTime => tsWithTime.join(''))
       .then(timeToUrl)
-      .then(({t, url}) => `[@${t}](${url})\n${captionTracker.get()}`.trim())
+      .then(({t, url}) => `[@${t}](${url})\n${maybeCaptions}`.trim())
       .then(copyText => navigator.clipboard.writeText(copyText))
       .then(_ => Util.toast(
-        `Copied time ${captionTracker.isActive ? 'with' : 'without'} captions`));
+        `Copied time ${maybeCaptions ? 'with' : 'without'} captions`));
   }
 
   function ensureSubtitles() {
@@ -43,18 +40,37 @@
   }
 
   function subtitleTrackToggle() {
-    captionTracker.reset(!captionTracker.isActive);
-    if (captionTracker.isActive) {
+    if (!window.captionTracker) {
+      window.captionTracker = new CaptionTracker(
+        100, '#player-container', '.captions-text');
       ensureSubtitles().then(_ => Util.toast('Captions on'));
+
+      // So long as you are staying in the same yt session,
+      // keep tracking subtitles but reset them when switching videos.
+      let curHref = document.location.href;
+      window.hrefObserver = new MutationObserver((_, self) => {
+        if (curHref != document.location.href) {
+          window.captionTracker.reset(true);
+          curHref = document.location.href;
+        }
+      });
+      window.hrefObserver.observe(document.body, {childList:true, subtree:true});
     } else {
+      window.hrefObserver.disconnect();
+      // necessary to deactivate observer
+      window.captionTracker.reset(false);
+
+      window.captionTracker = null;
+      window.hrefObserver = null;
+
       Util.toast('Captions off');
     }
   }
 
   window.onkeyup = document.onkeyup = Shortcut.init({
     a: [
-      Shortcut.fun('a', copyUrl),
-      Shortcut.fun('l', copyUrl),
+      Shortcut.fun('a', () => copyUrl(false)),
+      Shortcut.fun('c', () => copyUrl(true)),
 
       Shortcut.sel('i', '.ytp-miniplayer-button'),
       Shortcut.sel('i', '.ytp-miniplayer-expand-watch-page-button'),
