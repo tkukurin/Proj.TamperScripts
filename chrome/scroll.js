992a9944e1ee54830ec445b7d049a8ba88545f9c
Toni Kukurin <tkukurin@gmail.com>
2021-09-02 23:03:27 +0200

    add vim keybind history movement

--- chrome/scroll.js
+++ chrome/scroll.js
@@ -4,7 +4,7 @@ console.log('Vim shortcuts loaded');
 let state = null;
 
 window.addEventListener('keydown', e => {
-  const AMOUNT = 40;
+  const AMOUNT = 50;
 
   let scrollY = 0;
   let scrollX = 0;
@@ -21,8 +21,13 @@ window.addEventListener('keydown', e => {
     case 'l': scrollX = AMOUNT; break;
     case 'h': scrollX = -AMOUNT; break;
 
+    // full (doesn't work if scrollHeight undefined, e.g. YouTube)
     case 'G': scrollY = document.body.scrollHeight; break;
     case 'g': scrollY = (state === 'g') * -document.body.scrollHeight; break;
+
+    // history
+    case 'H': return window.history.back();
+    case 'L': return window.history.forward();
   }
 
   window.scrollBy(scrollX, scrollY);
