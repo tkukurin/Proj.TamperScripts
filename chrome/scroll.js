da48138faba48858e0cee7007d25da962c7f96a9
Toni Kukurin <tkukurin@gmail.com>
2021-09-02 23:10:32 +0200

    scollby rename

--- chrome/scroll.js
+++ chrome/scroll.js
@@ -1,29 +1,28 @@
 console.log('Vim shortcuts loaded');
 
+const SCROLL_BY = 50;
 const INPUT_TAGS = ['INPUT', 'TEXTAREA'];
-let state = null;
 
+let state = null;
 window.addEventListener('keydown', e => {
   if (INPUT_TAGS.indexOf(document.activeElement.tagName) >= 0) {
     return;
   }
 
-  const AMOUNT = 50;
-
   let scrollY = 0;
   let scrollX = 0;
   switch (e.key) {
     // up
-    case 'k': case 'w': scrollY = -AMOUNT; break;
-    case 'u': scrollY = -AMOUNT * 2; break;
+    case 'k': case 'w': scrollY = -SCROLL_BY; break;
+    case 'u': scrollY = -SCROLL_BY * 2; break;
 
     // down
-    case 'j': case 's': scrollY = AMOUNT; break;
-    case 'd': scrollY = AMOUNT * 2; break;
+    case 'j': case 's': scrollY = SCROLL_BY; break;
+    case 'd': scrollY = SCROLL_BY * 2; break;
 
     // left / right
-    case 'l': scrollX = AMOUNT; break;
-    case 'h': scrollX = -AMOUNT; break;
+    case 'l': scrollX = SCROLL_BY; break;
+    case 'h': scrollX = -SCROLL_BY; break;
 
     // full (doesn't work if scrollHeight undefined, e.g. YouTube)
     case 'G': scrollY = document.body.scrollHeight; break;
