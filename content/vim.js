29c31d93dd894602e36d3dcbfea620d6594e9063
Toni Kukurin <tkukurin@gmail.com>
2021-09-04 13:41:52 +0200

    kinda clumsy state machine with disabling option

--- content/vim.js
+++ content/vim.js
@@ -2,43 +2,37 @@ console.log('Vim shortcuts loaded');
 
 const SCROLL_BY = 50;
 
-class FollowState {
-  constructor() {
-    const _me = this;
-    _me.links = {};
-    _me.follow = '';
-    const chars = "asdfqwertzxcv";  // or const k = i + 10?
-    document.querySelectorAll('a').filter(Util.isVisible).forEach((el, i) => {
-      const k = chars[parseInt(i / chars.length)] + chars[i % chars.length];
-      el.append(Util.newEl('div', {className: '__vim_follow', innerHTML: k}));
-      _me.links[k] = el;
-    });
+class State {
+  next(e) {
+    if (e.altKey && e.key == 'q') {
+      return new NullState();
+    }
+    if (e.metaKey || e.altKey || e.ctrlKey) {
+      return this;
+    }
+    if (Util.isInput(document.activeElement) || e.key === 'Escape') {
+      return this.reset();
+    }
+    return this._next(e.key);
   }
-  next(c) {
-    this.follow += c;
-    if (this.follow.length >= 2) {
-      this.links[this.follow]?.click();
-      this.reset();
-      return new BaseState();
+  _next() {}
+  reset() { this._reset(); return new BaseState(); }
+  _reset() {}
+}
+
+class NullState extends State {
+  constructor() { super(); console.log("[VIM DBG] Disabling"); }
+  next(e) {
+    if (e.altKey && e.key == 'q') {
+      console.log("[VIM DBG] Enabling");
+      return this.reset();
     }
     return this;
   }
-  reset() { Object.values(this.links).forEach(e => e.lastChild.remove()); }
 }
 
-class GState {
-  next(c) {
-    const pageHeight = document.documentElement.scrollHeight;
-    switch(c) {
-      case 'g': window.scrollBy(0, -pageHeight); break;
-    }
-    return new BaseState();
-  }
-  reset() {}
-}
-
-class BaseState {
-  next(c) {
+class BaseState extends State {
+  _next(c) {
     const pageHeight = document.documentElement.scrollHeight;
     switch (c) {
       // up
@@ -55,7 +49,7 @@ class BaseState {
 
       // full
       case 'G': window.scrollBy(0, pageHeight); break;
-      case 'g': return new GState();
+      case 'g': return new GotoState();
 
       // history (return, no State change / fallthrough)
       case 'H': window.history.back(); break;
@@ -65,16 +59,43 @@ class BaseState {
     }
     return this;
   }
-  reset() {}
+  reset() { return this; }
+}
+
+class FollowState extends State {
+  constructor() {
+    super();
+    const _me = this;
+    _me.links = {};
+    _me.follow = '';
+    const chars = 'asdfqwertzxcv';  // or const k = i + 10?
+    document.querySelectorAll('a').filter(Util.isVisible).forEach((el, i) => {
+      const k = chars[parseInt(i / chars.length)] + chars[i % chars.length];
+      el.append(Util.newEl('div', {className: '__vim_follow', innerHTML: k}));
+      _me.links[k] = el;  // kinda implicitly assume < chars^2 links on screen
+    });
+  }
+  _next(c) {
+    this.follow += c;
+    if (this.follow.length >= 2) {
+      this.links[this.follow]?.click();
+      return this.reset();
+    }
+    return this;
+  }
+  _reset() { Object.values(this.links).forEach(e => e.lastChild.remove()); }
+}
+
+class GotoState extends State {
+  _next(c) {
+    const pageHeight = document.documentElement.scrollHeight;
+    switch(c) {
+      case 'g': window.scrollBy(0, -pageHeight); break;
+    }
+    return this.reset();
+  }
 }
 
 let s = new BaseState();
-window.addEventListener('keydown', e => {
-  if (Util.isInput(document.activeElement) || e.key === 'Escape') {
-    s.reset();
-    return (s = new BaseState());
-  }
-
-  s = s.next(e.key);
-}, false);
+window.addEventListener('keydown', e => (s = s.next(e)), false);
 
