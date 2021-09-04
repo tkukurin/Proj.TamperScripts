8ccb9136ec551995ef8ee5176a2dd9ef8677c43d
Toni Kukurin <tkukurin@gmail.com>
2021-09-04 11:16:42 +0200

    State based classes

--- chrome/vim.js
+++ chrome/vim.js
@@ -8,30 +8,6 @@ NodeList.prototype.map = Array.prototype.map;
 
 const SCROLL_BY = 50;
 
-// Awful global state wrapper. Works while script is smallish.
-const State = {
-  _repr: null, _val: '', _attached: {},
-
-  attach: function(k, v) { return this._attached[k] = v; },
-  attached: function(k) { return this._attached[k]; },
-  is: function(c) { return this._repr && this._repr.startsWith(c); },
-  next: function(c) {
-    if (this.is('f')) {
-      this._val += c;
-      return this._val;
-    } else {
-      this._repr = this._repr == c ? null : c;
-      return this._repr;
-    }
-  },
-  reset: function(attachedCallback = ([k, v]) => {}) {
-    Object.entries(this._attached).forEach(attachedCallback);
-    this._repr = null;
-    this._val = '';
-    this._attached = {};
-  }
-}
-
 const Util = {
   mk: (type, propsOrString) => {
     const el = document.createElement(type);
@@ -57,50 +33,80 @@ const Util = {
   }
 }
 
+class FollowState {
+  constructor() {
+    const _me = this;
+    _me.links = {};
+    _me.follow = '';
+    const chars = "asdfqwertzxcv";  // or const k = i + 10?
+    document.querySelectorAll('a').filter(Util.isVisible).forEach((el, i) => {
+      const k = chars[parseInt(i / chars.length)] + chars[i % chars.length];
+      el.append(Util.mk('div', {className: '__vim_follow', innerHTML: k}));
+      _me.links[k] = el;
+    });
+
+    _me.reset = () => Object.values(_me.links).forEach(e => e.lastChild.remove());
+  }
+  next(c) {
+    this.follow += c;
+    if (this.follow.length >= 2) {
+      this.links[this.follow]?.click();
+      this.reset();
+      return new BaseState();
+    }
+    return this;
+  }
+}
+
+class GState {
+  next(c) {
+    const pageHeight = document.documentElement.scrollHeight;
+    switch(c) {
+      case 'g': window.scrollBy(0, -pageHeight); break;
+    }
+    return new BaseState();
+  }
+  reset() {}
+}
+
+class BaseState {
+  next(c) {
+    const pageHeight = document.documentElement.scrollHeight;
+    switch (c) {
+      // up
+      case 'k': case 'w': window.scrollBy(0, -SCROLL_BY); break;
+      case 'u': window.scrollBy(0, -SCROLL_BY * 2); break;
+
+      // down
+      case 'j': case 's': window.scrollBy(0, SCROLL_BY); break;
+      case 'd': window.scrollBy(0, SCROLL_BY * 2); break;
+
+      // left / right
+      case 'l': window.scrollBy(SCROLL_BY, 0); break;
+      case 'h': window.scrollBy(-SCROLL_BY, 0); break;
+
+      // full
+      case 'G': window.scrollBy(0, pageHeight); break;
+      case 'g': return new GState();
+
+      // history (return, no State change / fallthrough)
+      case 'H': window.history.back(); break;
+      case 'L': window.history.forward(); break;
+
+      case 'f': return new FollowState();
+    }
+    return this;
+  }
+  reset() {}
+}
+
+let s = new BaseState();
 window.addEventListener('keydown', e => {
   if (Util.isInput(document.activeElement) || e.key === 'Escape') {
-    return State.reset(([_, el]) => el?.lastChild?.remove());
+    s.reset();
+    return (s = new BaseState());
   }
 
-  if (State.is('f')) {
-    const follow = State.next(e.key);
-    return follow.length >= 2 && State.reset(([k, el]) =>
-      (k === follow && el?.click()) || el?.lastChild?.remove());
-  }
-
-  const pageHeight = document.documentElement.scrollHeight;
-  switch (e.key) {
-    // up
-    case 'k': case 'w': window.scrollBy(0, -SCROLL_BY); break;
-    case 'u': window.scrollBy(0, -SCROLL_BY * 2); break;
-
-    // down
-    case 'j': case 's': window.scrollBy(0, SCROLL_BY); break;
-    case 'd': window.scrollBy(0, SCROLL_BY * 2); break;
-
-    // left / right
-    case 'l': window.scrollBy(SCROLL_BY, 0); break;
-    case 'h': window.scrollBy(-SCROLL_BY, 0); break;
-
-    // full
-    case 'G': window.scrollBy(0, pageHeight); break;
-    case 'g': window.scrollBy(0, State.is('g') * -pageHeight); break;
-
-    // history (return, no State change / fallthrough)
-    case 'H': return window.history.back();
-    case 'L': return window.history.forward();
-
-    // follow
-    case 'f':
-      document.querySelectorAll('a').filter(Util.isVisible).forEach((el, i) => {
-        const chars = "asdfqwertzxcv";  // or const k = i + 10?
-        const k = chars[parseInt(i / chars.length)] + chars[i % chars.length];
-        el.append(Util.mk('div', {className: '__vim_follow', innerHTML: k}));
-        State.attach(k, el);
-      });
-      break;
-  }
-
-  State.next(e.key);
+  s = s.next(e.key);
 }, false);
 
