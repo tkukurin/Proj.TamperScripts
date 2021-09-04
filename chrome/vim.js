6857ad38560e0fbf5dab92efacc2d378aac2ab51
Toni Kukurin <tkukurin@gmail.com>
2021-09-04 09:26:38 +0200

    add css file with cleanup

--- chrome/vim.js
+++ chrome/vim.js
@@ -11,90 +11,63 @@ const INPUT_TAGS = ['INPUT', 'TEXTAREA'];
 
 // Awful global state wrapper. Works while script is smallish.
 const State = {
-  repr: null,
-  attached: {},
-  val: '',
+  _repr: null, _val: '', _attached: {},
 
-  is: function(c) {
-    return this.repr && this.repr.startsWith(c);
-  },
+  attach: function(k, v) { return this._attached[k] = v; },
+  attached: function(k) { return this._attached[k]; },
+  is: function(c) { return this._repr && this._repr.startsWith(c); },
   next: function(c) {
     if (this.is('f')) {
-      this.val += c;
-      return parseInt(this.val);
+      this._val += c;
+      return parseInt(this._val);
     } else {
-      this.repr = this.repr == c ? null : c;
-      return this.repr;
+      this._repr = this._repr == c ? null : c;
+      return this._repr;
     }
   },
   reset: function() {
-    Object.values(this.attached).forEach(
+    Object.values(this._attached).forEach(
       el => el.children[el.children.length - 1].remove());
-    this.repr = null;
-    this.attached = {};
-    this.val = '';
+    this._repr = null;
+    this._val = '';
+    this._attached = {};
   }
 }
 
-function isVisible(el) {
-  // cf. https://stackoverflow.com/questions/19669786/check-if-element-is-visible-in-dom
-  const rect = el.getBoundingClientRect();
-  const W = document.body.offsetWidth;
-  const H = document.body.offsetHeight;
-  const visible = rect.width && rect.height;
-  const inBounds = 0 <= rect.x && rect.x <= W && 0 <= rect.y && rect.y <= H;
-  return visible && inBounds;
-}
-
-const Util = {};
-Util.newEl = (type, propsOrString) => {
-  const el = document.createElement(type);
-  if (typeof(propsOrString) === 'string') {
-    el.innerHTML = propsOrString;
-  } else {
-    for (let prop in propsOrString) {
-      el[prop] = propsOrString[prop];
+const Util = {
+  mk: (type, propsOrString) => {
+    const el = document.createElement(type);
+    if (typeof(propsOrString) === 'string') {
+      el.innerHTML = propsOrString;
+    } else {
+      for (let prop in propsOrString) {
+        el[prop] = propsOrString[prop];
+      }
     }
+    return el;
+  },
+  isInput: el => (
+    INPUT_TAGS.indexOf(el.tagName) >= 0 || el.contentEditable === 'true'),
+  isVisible: el => {
+    // cf. https://stackoverflow.com/questions/19669786/check-if-element-is-visible-in-dom
+    const rect = el.getBoundingClientRect();
+    const W = document.body.offsetWidth;
+    const H = document.body.offsetHeight;
+    const visible = rect.width && rect.height;
+    const inBounds = 0 <= rect.x && rect.x <= W && 0 <= rect.y && rect.y <= H;
+    return visible && inBounds;
   }
-  return el;
 }
 
-/* some style
-  position: absolute;
-  color: #302505 !important;
-  background-color: #ffd76e !important;
-  border-radius: 2px !important;
-  padding: 2px !important;
-  font-size: 8pt !important;
-  font-weight: 500 !important;
-  text-transform: uppercase !important;
-  border: 1px solid #ad810c;
-  display: inline-block !important;
-  vertical-align: middle !important;
-  text-align: center !important;
-  box-shadow: 2px 2px 1px rgba(0,0,0,0.25) !important;
-*/
 window.addEventListener('keydown', e => {
-  if (INPUT_TAGS.indexOf(document.activeElement.tagName) >= 0
-    || document.activeElement.contentEditable === 'true') {
-    return;
-  }
-
-  if (e.key === 'Escape') {
+  if (Util.isInput(document.activeElement) || e.key === 'Escape') {
     return State.reset();
   }
 
   if (State.is('f')) {
     const follow = State.next(e.key);
-    if (follow >= 100) {
-      return State.reset();
-    }
-    const link = State.attached[follow];
-    if (link) {
-      link.click();
-      State.reset();
-    }
-    return;
+    State.attached(follow)?.click();
+    return follow >= 10 && State.reset();
   }
 
   const pageHeight = document.documentElement.scrollHeight;
@@ -121,15 +94,10 @@ window.addEventListener('keydown', e => {
 
     // follow
     case 'f':
-      document.querySelectorAll('a').filter(isVisible).forEach((el, ix) => {
-        const follow = ix + 10;
-        // TODO move the style to soe css file or sth.
-        el.append(Util.newEl('div', {
-          style: `position: absolute; background: #ffd76e; color: #302505;
-          border: 1px solid #ad810c; display: inline-block; text-align: center`,
-          innerHTML: follow
-        }));
-        State.attached[follow] = el;
+      document.querySelectorAll('a').filter(Util.isVisible).forEach((el, i) => {
+        const k = i + 10;  // start from double digits (90 labels total).
+        el.append(Util.mk('div', {className: '__vim_follow', innerHTML: k}));
+        State.attach(k, el);
       });
       break;
   }
